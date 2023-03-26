31d9ab842964e6fdfaf1ff216f1f5419069ea06e
Josh <joshuacofie@outlook.com>
2023-03-26 20:14:34 -0300

    #adding codes which will allow you select your answer

---
+++ style.js
@@ -0,0 +1,20 @@
+const questions = [
+    {
+        questions: "What programming language does a frontend developer has to learn first?",
+        answers: [
+            { text: "Python, React", correct: false},
+            { text: "HTML, Css, JavaScrpit", correct: true},
+            { text: "Powe BI, Java, Go", correct: false},
+            { text: "Swift, Perl Rust", correct: false},
+        ]
+    },
+    {
+        questions: "What is the most difficult programming language to learn?",
+        answers: [
+            { text: "C++", correct: false},
+            { text: "Python", correct: false},
+            { text: "Malboge", correct: true},
+            { text: "Swift", correct: false},
+        ]
+    },
+];
